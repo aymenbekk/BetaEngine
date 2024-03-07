944142de4d8b6f829dfea607e55924f0b71f7693
aymenbekk <a.bekkouche@esi-sba.dz>
2024-03-07 13:35:34 -0800

    first commit

---
+++ Beta_Engine/Beta_Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Beta_Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Beta_Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
